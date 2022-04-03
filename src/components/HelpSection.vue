cca823b2c4225907ac2781d271b917c85712fad3
olaide <olaide.olanipekun@yahoo.com>
2022-04-03 02:58:51 +0100

    added Help section

---
+++ src/components/HelpSection.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="container help-section-bg">
+    <div class="inner-container help-section-content">
+      <h2>How can we help you?</h2>
+      <h5>
+        Let us know who you are and what you're looking for, and we'll help get
+        you to the right place.
+      </h5>
+      <div class="form">
+        <p>I am an individual and i want to learn</p>
+        <CurvedButton
+          text="Get involved now!"
+          color="white"
+          backgroundColor="rgb(170, 35, 35)"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import CurvedButton from "../components/CurvedButton.vue";
+export default {
+  components: {
+    CurvedButton,
+  },
+};
+</script>
+
+
+<style lang="scss">
+.help-section-bg {
+  background: #ececec;
+  padding-top: 2rem;
+
+  .help-section-content {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: flex-start;
+
+    h5 {
+      margin-top: 0.8rem;
+    }
+  }
+
+  .form {
+    display: flex;
+    width: 100%;
+    flex-direction: row;
+    align-items: center;
+    background: #fff;
+    justify-content: center;
+    margin: 2rem 0;
+    padding: 1.5rem 0;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,17 @@
 <template>
   <HeroSection />
+  <HelpSection />
 </template>
 
 <script>
 // @ is an alias to /src
 import HeroSection from "../components/HeroSection.vue";
+import HelpSection from "../components/HelpSection.vue";
 export default {
   name: "HomeView",
   components: {
     HeroSection,
+    HelpSection,
   },
 };
 </script>
